b06d523d2702e7361302dba6f986f800bb7b5743
Ludmila Pomohaibo <pomohaibo@gmail.com>
2018-10-25 10:10:06 +0300

    made property transfer from app/component to navbar.component

--- frontend/src/app/nav-bar/nav-bar.component.ts
+++ frontend/src/app/nav-bar/nav-bar.component.ts
@@ -1,18 +1,14 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
     selector: 'app-nav-bar',
     templateUrl: './nav-bar.component.html',
     styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent implements OnInit {
-    logo: string = '/assets/img/logo.png';
-    clinicName: string = 'BabyMed';
+export class NavBarComponent{
+    @Input() menuItems;
 
     constructor() {
-    }
-
-    ngOnInit() {
     }
 
 }
