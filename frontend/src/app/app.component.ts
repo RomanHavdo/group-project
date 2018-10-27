f490d52e2431027aebb363544b0275db35740519
Roman Havdo <32357324+RomanHavdo@users.noreply.github.com>
2018-10-27 18:12:02 +0300

    add carousel

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.css'],
+    
 })
-export class AppComponent {
 
+export class AppComponent {
+    images=[
+        'assets/img/slides/slide1.jpg',
+        'assets/img/slides/slide2.jpg',
+        'assets/img/slides/slide3.jpg',
+        'assets/img/slides/slide4.jpg',
+        'assets/img/slides/slide5.jpg',
+    ];
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,28 +1,32 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms'
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
-import {AppComponent} from './app.component';
-import {NavBarComponent} from './nav-bar/nav-bar.component';
-import {FooterComponent} from './footer/footer.component';
-import {AppointmentWindowComponent} from './appointment-window/appointment-window.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { AppointmentWindowComponent } from './appointment-window/appointment-window.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { CarouselModule } from './carousel/carousel.module';
 
 @NgModule({
     declarations: [
         AppComponent,
         NavBarComponent,
         FooterComponent,
-        AppointmentWindowComponent
+        AppointmentWindowComponent,
     ],
     imports: [
         BrowserModule,
         FormsModule,
-        NgbModule
+        NgbModule,
+        CarouselModule
     ],
     providers: [],
     bootstrap: [AppComponent]
 })
+
 export class AppModule {
 }
 
