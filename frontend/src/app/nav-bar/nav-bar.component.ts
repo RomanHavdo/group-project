66522127c4f042858a36f3ac2b7ec208b965be5a
Ludmila Pomohaibo <pomohaibo@gmail.com>
2018-11-04 13:12:04 +0200

    Made form validation

--- frontend/src/app/nav-bar/nav-bar.component.ts
+++ frontend/src/app/nav-bar/nav-bar.component.ts
@@ -1,19 +1,54 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import {User} from "./user";
+import {el} from "@angular/platform-browser/testing/src/browser_util";
+import {current} from "codelyzer/util/syntaxKind";
+
 
 @Component({
     selector: 'app-nav-bar',
     templateUrl: './nav-bar.component.html',
     styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent{
+export class NavBarComponent {
     @Input() menuItems;
     closeResult: string;
+    newUser: User[] = [{
+        name: "Petia",
+        dateOfBirth: "12-09-1953",
+        sex: "Чоловіча",
+        homeAddress: "NY, Manhattan, 22",
+        emailAddress: "admin",
+        phoneNumber: "+380001234567",
+        password: "admin"
+    }];
+    authError: string;
+    sex: string[] = ["Чоловіча", "Жіноча"];
 
     constructor(private modalService: NgbModal) {
     }
 
+    checkUser(logInEmail, logInPassword) {
+        let foundUser =
+            this.newUser.filter(item => item.emailAddress === logInEmail.viewModel && item.password === logInPassword.viewModel)[0];
+        if (foundUser) {
+            this.authError = '';
+            console.log('in');
+            this.modalService.dismissAll();
+        } else {
+            this.authError = 'Такого користувача не існує!';
+        }
+    }
+
+
+    onSubmit(name, dateofbirth,sex, homeAddress, emailAddress, phoneNumber, password) {
+        this.newUser.push(new User(name.viewModel, dateofbirth.viewModel,sex.value, homeAddress.viewModel, emailAddress.viewModel, phoneNumber.viewModel, password.viewModel));
+        console.log(this.newUser);
+    }
+
+
     open(content) {
+        this.authError = '';
         this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
             this.closeResult = `Closed with: ${result}`;
         }, (reason) => {
---
+++ frontend/src/app/nav-bar/user.ts
@@ -0,0 +1,12 @@
+export class User {
+    constructor(
+        public name: string,
+        public dateOfBirth: string,
+        public sex:string,
+        public homeAddress: string,
+        public emailAddress: string,
+        public phoneNumber: string,
+        public password: string,
+    ) {
+    }
+}
