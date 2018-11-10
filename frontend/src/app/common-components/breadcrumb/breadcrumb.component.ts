81411e597de7981d223f917d58a25cff3a722f75
Ludmila Pomohaibo <pomohaibo@gmail.com>
2018-11-10 23:29:11 +0200

    Add initial breadcrumb detecting in breadcrumb component

--- frontend/src/app/common-components/breadcrumb/breadcrumb.component.ts
+++ frontend/src/app/common-components/breadcrumb/breadcrumb.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, NavigationEnd, Router} from "@angular/router";
+import { filter } from 'rxjs/operators';
+import {BreadCrumb} from "../../interfaces/breadCrumb";
 
 @Component({
-  selector: 'app-breadcrumb',
-  templateUrl: './breadcrumb.component.html',
-  styleUrls: ['./breadcrumb.component.css']
+    selector: 'app-breadcrumb',
+    templateUrl: './breadcrumb.component.html',
+    styleUrls: ['./breadcrumb.component.css']
 })
 export class BreadcrumbComponent implements OnInit {
+    breadcrumbs: Array<BreadCrumb>;
 
-  constructor() { }
+    constructor(private router: Router, private route: ActivatedRoute) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.breadcrumbs = [{label:"title", url:"weffe"}];
+        this.initBreadcrumbsListening();
+    }
 
+    private initBreadcrumbsListening() {
+        this.router.events
+            .pipe(filter(event => event instanceof NavigationEnd))
+            .subscribe(event => {  // note, we don't use event
+                this.breadcrumbs = [];
+                let currentRoute = this.route.root,
+                    url = '';
+                do {
+                    let childrenRoutes = currentRoute.children;
+                    currentRoute = null;
+                    childrenRoutes.forEach(route => {
+                        if (route.outlet === 'primary') {
+                            let routeSnapshot = route.snapshot;
+                            url += '/' + routeSnapshot.url.map(segment => segment.path).join('/');
+                            this.breadcrumbs.push({
+                                label: route.snapshot.data.breadcrumb,
+                                url: url
+                            });
+                            console.log(JSON.stringify(this.breadcrumbs));
+                            currentRoute = route;
+                        }
+                    })
+                } while (currentRoute);
+            });
+    }
 }
