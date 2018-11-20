e98fcabfc63d4536ec26a0ef6a20f1b91c83e3a1
Ludmila Pomohaibo <pomohaibo@gmail.com>
2018-11-20 15:15:25 +0200

    Changed appointment form in modal window; Added success notification on sending and error if contact fields are empty; Uploading user information to appointment forms if user logged in;

--- frontend/src/app/Pages/register-reception/register-reception.component.ts
+++ frontend/src/app/Pages/register-reception/register-reception.component.ts
@@ -1,42 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpService} from '../../http.service';
-import { ActivatedRoute, Router} from '@angular/router';
-import { AuthService} from '../../auth.service';
+import {Component, OnInit} from '@angular/core';
+import {HttpService} from '../../http.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {AuthService} from '../../auth.service';
+import {User} from "../../interfaces/user";
 
 
 @Component({
-  selector: 'app-register-reception',
-  templateUrl: './register-reception.component.html',
-  styleUrls: ['./register-reception.component.css']
+    selector: 'app-register-reception',
+    templateUrl: './register-reception.component.html',
+    styleUrls: ['./register-reception.component.css']
 })
 export class RegisterReceptionComponent implements OnInit {
 
-  constructor(private _http:HttpService, private route: ActivatedRoute, private router: Router, private _auth:AuthService) { }
-   queryId:number;
-   doctorInfo:any;
-   isLogin:boolean;
-   staticAlertClosed = false;
-  ngOnInit() {
-    this.route.params.subscribe(params => {
-      this.queryId = params.id;
-    });
-    this._http.getDoctorById(this.queryId).subscribe((res) => {
+    constructor(private _http: HttpService, private route: ActivatedRoute, private router: Router, private _auth: AuthService) {
+    }
 
-      this.doctorInfo = res[0];
-    });
-     if(this._auth.authUser == null){
-     this.isLogin = false;
-     }
-     else if(this._auth.authUser){
-      this.isLogin = true;
-     }
-  }
+    queryId: number;
+    doctorInfo: any;
+    userInf: any = {
+        name: '',
+        email: '',
+        phone: '',
+    };
+    isLogin: boolean;
+    staticAlertClosed = false;
 
-  showMsg(){
-    this.staticAlertClosed = true;
-   setTimeout(() => {
-    this.router.navigate(['/']);
-   }, 2000)
-  }
+    ngOnInit() {
+        this.route.params.subscribe(params => {
+            this.queryId = params.id;
+        });
+        this._http.getDoctorById(this.queryId).subscribe((res) => {
+
+            this.doctorInfo = res[0];
+        });
+        if (this._auth.authUser == null) {
+            this.isLogin = false;
+        }
+        else if (this._auth.authUser) {
+            this.isLogin = true;
+            this.userInf = this._auth.authUser;
+        }
+    }
+
+    showMsg() {
+        this.staticAlertClosed = true;
+        setTimeout(() => {
+            this.router.navigate(['/']);
+        }, 2000)
+    }
 
 }
--- frontend/src/app/appointment-window/appointment-window.component.ts
+++ frontend/src/app/appointment-window/appointment-window.component.ts
@@ -1,9 +1,8 @@
 import {Component} from '@angular/core';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
-import { Router } from '@angular/router';
-
-
-
+import {Router} from '@angular/router';
+import {ToastrService} from "../toastr.service";
+import {AuthService} from "../auth.service";
 
 @Component({
     selector: 'app-appointment-window',
@@ -13,23 +12,44 @@ import { Router } from '@angular/router';
 export class AppointmentWindowComponent {
     closeResult: string;
     public visible = true;
+    userInfo: any = {
+        name: '',
+        email: '',
+        phone: '',
+    };
 
-    constructor(private modalService: NgbModal, private router: Router) {
+    sendMessage(email, phone) {
+        if (!email && !phone) {
+            this.toastrService.Error('Повідомлення не надіслано. Додайте телефон або пошту');
+        } else {
+            this.toastrService.Success('Повідомлення надіслано');
+        }
+    }
+
+    checkUserInformation() {
+        if (this.authService.authUser !== null) {
+            this.userInfo = this.authService.authUser;
+        }
+    }
+
+    constructor(private modalService: NgbModal, private router: Router, private toastrService: ToastrService, private authService: AuthService) {
         this.router.events.subscribe((event) => {
             if (this.router.url !== '/') {
                 this.visible = false;
             } else {
                 this.visible = true;
             }
-      });
+        });
     }
 
     open(content) {
+        this.checkUserInformation();
         this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
             this.closeResult = `Closed with: ${result}`;
         }, (reason) => {
             // this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
         });
     }
+
 }
 
