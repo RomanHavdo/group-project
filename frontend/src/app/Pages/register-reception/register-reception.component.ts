035385a5c36bbc23bad8d87690c354d0a2f0b7fe
Libomyr Struk <strukliubomyr@gmail.com>
2018-11-21 00:17:09 +0200

    Create service

--- frontend/src/app/Pages/register-reception/register-reception.component.ts
+++ frontend/src/app/Pages/register-reception/register-reception.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {AuthService} from '../../auth.service';
 import {ToastrService} from "../../toastr.service";
+import {ChangerService} from '../../changer.service';
 
 @Component({
   selector: 'app-register-reception',
@@ -20,11 +21,12 @@ export class RegisterReceptionComponent implements OnInit {
   isLogin: boolean;
   staticAlertClosed = false;
 
-  constructor(private route: ActivatedRoute, private router: Router, private _auth: AuthService, private toastrService: ToastrService) {
+  constructor(private route: ActivatedRoute, private router: Router, private _auth: AuthService, private toastrService: ToastrService, private _change:ChangerService) {
     this.doctorInfo = this.route.snapshot.data['doctorInfo'][0];
   }
 
   ngOnInit() {
+    this._change.emitConfig(false);
     if (this._auth.authUser == null) {
       this.isLogin = false;
     } else if (this._auth.authUser) {
@@ -37,9 +39,7 @@ export class RegisterReceptionComponent implements OnInit {
     this.toastrService.Success('Ви успішно записались до лікаря!');
     setTimeout(() => {
       this.router.navigate(['/']);
-      setTimeout(() => {
-        location.reload();
-      })
+      this._change.emitConfig(true);
     }, 2000)
   }
 
